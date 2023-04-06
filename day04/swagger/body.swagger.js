805e698dd6dcd3b34da4487fbdbb4b9cdd84293c
kyungmin kim <ee5201@naver.com>
2023-04-06 22:33:30 +0900

    Updatehomework

---
+++ day04/swagger/body.swagger.js
@@ -0,0 +1,57 @@
+/**
+ * @swagger
+ * /boards:
+ *   get:
+ *     summary: 게시글 가져오기 
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         content:
+ *            application/json:
+ *                schema:
+ *                    type: array
+ *                    items: 
+ *                    example:
+ *                          
+ *                            - name: 아메리카노
+ *                              Kcal: 3
+ *                          
+ *                            - name: 카페라뗴
+ *                              Kcal: 10
+ *                          
+ *                            - name: 콜드브루
+ *                              Kcal: 15
+ *                          
+ *                            - name: 카페모카
+ *                              Kcal: 50
+ *                          
+ *                            - name: 돌체라뗴
+ *                              Kcal: 500
+ *                          
+ *                            - name: 카라멜라떼
+ *                              Kcal: 200
+ *                          
+ *                            - name: 바닐라라뗴
+ *                              Kcal: 20
+ *                          
+ *                            - name: 에스프레소
+ *                              Kcal: 1
+ *                          
+ *                            - name: 디카페인
+ *                              Kcal: 5
+ *                          
+ *                            - name: 오트라뗴
+ *                              Kcal: 300
+ *                
+ *                        
+ *                              
+ *         
+ *                        
+ *                            
+ *              
+ *                    
+ *                        
+ */
+
+
